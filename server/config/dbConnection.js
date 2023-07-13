6f33aa04f39bc8cc177acacf44e760595a1b21d9
evy1-ua <evy1@alu.ua.es>
2023-07-13 13:51:09 +0200

    a

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -5,6 +5,7 @@ dotenv.config({ path: path.join(__dirname, '../.env') });
  const dbConfig = {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
+      port: process.env.DB_PORT,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
  };
